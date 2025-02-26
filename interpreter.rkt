02ebbdb0a7b5c8cbfa79ed09f0e967fff1c942b2
AnhP03 <63649337+duyanh131212@users.noreply.github.com>
2025-02-26 02:38:53 -0500

    add cmts

--- interpreter.rkt
+++ interpreter.rkt
@@ -36,6 +36,7 @@
 ; rightoperand
 (define rightoperand caddr)
 
+; getval from state
 (define (getval name state)
   (let* ([vars (statevars state)]
          [vals (statevals state)])
@@ -46,12 +47,13 @@
                                         (car vals))]
             [else                   (getval name (list (cdr vars) (cdr vals)))])))
 
+; add
 (define (add name val state)
   (cond
     [(contains? name (statevars state))     (error "alr existed")]
     [else                                   (list   (cons name (statevars state)) 
                                                     (cons val (statevals state)))]))
-
+; remove
 (define (remove name state)
   (removecps name state (lambda (v) v)))
 
@@ -65,9 +67,10 @@
                                                         (lambda (s) 
                                                             (ret (list  (cons (car (statevars state)) (statevars s)) 
                                                                         (cons (car (statevals state)) (statevals s))))))]))
-
+; declare
 (define (declare name state) (add name '() state))
 
+; assign
 (define (assign name val state)
   (cond
     [(contains? name (statevars state)) (add name val (remove name state))]
@@ -78,41 +81,47 @@
     (lambda (expr state)
         (cond
         [(null? expr)       (error "called M_value on a null value")]
+        [(assign? expr)     (M_value (cadr expr) (M_state_assign expr state))]
         [(arith? expr)      (M_int expr state)]
         [(bool? expr)       (M_boolean expr state)]
         [(not (list? expr)) (getval expr state)])))
 
 (define (signed? expr) (eq? (len expr 0) 2))
-(define (bool? expr) 
+(define (bool? expr)
+  (or (contains? expr '(true false))
     (and    (list? expr) 
-            (contains? (operator expr) '(true false == < > <= >= || &&))))
+            (contains? (operator expr) '(== < > <= >= || &&)))))
 (define (arith? expr) 
     (or (number? expr) 
         (and    (list? expr) 
                 (contains? (operator expr) '(+ - * / %)))))
+(define (assign? expr)
+  (and (list? expr)
+       (eq? (operator expr) '=)))
 
+; M_int
 (define (M_int expr state)
   (cond
     [(not (list? expr))             (if (number? expr)
                                         expr
                                         (getval expr state))]
     [(and   (eq? '+ (operator expr))
-            (not (signed? expr)))   (+  (M_int (leftoperand expr) state)
-                                        (M_int (rightoperand expr) state))]
+            (not (signed? expr)))   (+  (M_value (leftoperand expr) state)
+                                        (M_value (rightoperand expr) state))]
     [(and   (eq? '+ (operator expr))
-            (signed? expr))         (M_int (leftoperand expr) state)]
+            (signed? expr))         (M_value (leftoperand expr) state)]
     [(and   (eq? '- (operator expr))
-            (not (signed? expr)))   (-  (M_int (leftoperand expr) state)
-                                        (M_int (rightoperand expr) state))]
+            (not (signed? expr)))   (-  (M_value (leftoperand expr) state)
+                                        (M_value (rightoperand expr) state))]
     [(and   (eq? '- (operator expr))
             (signed? expr))         (-  0 
-                                        (M_int (leftoperand expr) state))]
-    [(eq? '* (operator expr))       (*  (M_int (leftoperand expr) state)
-                                        (M_int (rightoperand expr) state))]
-    [(eq? '/ (operator expr))       (quotient   (M_int (leftoperand expr) state)
-                                                (M_int (rightoperand expr) state))]
-    [(eq? '% (operator expr))       (remainder  (M_int (leftoperand expr) state)
-                                                (M_int (rightoperand expr) state))]))
+                                        (M_value (leftoperand expr) state))]
+    [(eq? '* (operator expr))       (*  (M_value (leftoperand expr) state)
+                                        (M_value (rightoperand expr) state))]
+    [(eq? '/ (operator expr))       (quotient   (M_value (leftoperand expr) state)
+                                                (M_value (rightoperand expr) state))]
+    [(eq? '% (operator expr))       (remainder  (M_value (leftoperand expr) state)
+                                                (M_value (rightoperand expr) state))]))
 ; M_boolean
 (define M_boolean
   (lambda (expr state)
@@ -121,21 +130,21 @@
       [(eq? 'true expr)             #t]
       [(eq? 'false expr)            #f]
       [(not (list? expr))           (getval expr state)]
-      [(eq? '! (operator expr))     (not (M_boolean (leftoperand expr) state))]
-      [(eq? '== (operator expr))    (eq?    (M_int (leftoperand expr) state)
-                                            (M_int (rightoperand expr) state))]
-      [(eq? '< (operator expr))     (<      (M_int (leftoperand expr) state)
-                                            (M_int (rightoperand expr) state))]
-      [(eq? '> (operator expr))     (>      (M_int (leftoperand expr) state)
-                                            (M_int (rightoperand expr) state))]
-      [(eq? '<= (operator expr))    (<=     (M_int (leftoperand expr) state)
-                                            (M_int (rightoperand expr) state))]
-      [(eq? '>= (operator expr))    (>=     (M_int (leftoperand expr) state)
-                                            (M_int (rightoperand expr) state))]
-      [(eq? '|| (operator expr))    (or     (M_boolean (leftoperand expr) state)
-                                            (M_boolean (rightoperand expr) state))]
-      [(eq? '&& (operator expr))    (and    (M_boolean (leftoperand expr) state)
-                                            (M_boolean (rightoperand expr) state))]
+      [(eq? '! (operator expr))     (not (M_value (leftoperand expr) state))]
+      [(eq? '== (operator expr))    (eq?    (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
+      [(eq? '< (operator expr))     (<      (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
+      [(eq? '> (operator expr))     (>      (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
+      [(eq? '<= (operator expr))    (<=     (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
+      [(eq? '>= (operator expr))    (>=     (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
+      [(eq? '|| (operator expr))    (or     (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
+      [(eq? '&& (operator expr))    (and    (M_value (leftoperand expr) state)
+                                            (M_value (rightoperand expr) state))]
       )))
 
 ; M_state_stmt_list, entry point for all parse tree
@@ -164,6 +173,7 @@
   (cond
     [(null? stmt)   (error "cannot return null")]
     [else           (process-output (M_value (cadr stmt) state))]))
+
 
 (define name cadr)
 (define expr caddr)
@@ -196,8 +206,10 @@
 (define stmt2 cadddr)
 (define (M_state_if stmt state)
   (cond
-    [(M_value (condition stmt) state)   (M_state_stmt (stmt1 stmt) state)]
-    [(eq? (len stmt 0) 4)               (M_state_stmt (stmt2 stmt) state)]
+    [(M_value (condition stmt) state)   (M_state_stmt (stmt1 stmt)
+                                                      (M_state_stmt (condition stmt) state))]
+    [(eq? (len stmt 0) 4)               (M_state_stmt (stmt2 stmt)
+                                                      (M_state_stmt (condition stmt) state))]
     [else                               state]))
 
 ; M_state_while
@@ -205,7 +217,7 @@
 (define loop_body caddr)
 (define (M_state_while stmt state)
   (cond
-    [(M_boolean (loop_condition stmt) state)    (M_state_while  stmt 
+    [(M_value (loop_condition stmt) state)    (M_state_while  stmt 
                                                                 (M_state_stmt   (loop_body stmt) 
                                                                                 (M_state_stmt (loop_condition stmt) state)))]
     [else                                       state]))
@@ -238,11 +250,11 @@
 (interpret "tests/20.txt")
 
 ;; extra tests
-(interpret "tests/21.txt")
+(interpret "tests/21.txt");ok
 (interpret "tests/22.txt")
 ;(interpret "tests/23.txt")
-;(interpret "tests/24.txt")
-;(interpret "tests/25.txt")
+(interpret "tests/24.txt")
+(interpret "tests/25.txt");ok
 ;(interpret "tests/26.txt")
-;(interpret "tests/27.txt")
+;(interpret "tests/27.txt");infinite loop vi k update global state
 ;(interpret "tests/28.txt")
